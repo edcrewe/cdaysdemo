13cbb53516820faf5201e652f13621ba24c39025
edcrewe <ed.crewe@enterprisedb.com>
2026-01-04 14:39:58 +0000

    add protovalidate to illustrate its installation and use

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Create directory for Google APIs
+# Create directory for Google API
+rm -fr generated
 mkdir -p googleapis/google/api
 mkdir -p generated/go
 
@@ -11,8 +12,10 @@ curl -o googleapis/google/api/http.proto \
 https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto
 curl -o googleapis/google/api/httpbody.proto \
 https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/httpbody.proto
-
-export GOOGLEAPIS_DIR=googleapis
+curl -L -o protovalidate.tar.gz https://github.com/bufbuild/protovalidate/archive/refs/tags/v1.0.0.tar.gz
+tar -xzf protovalidate.tar.gz --strip-components=2 "protovalidate-1.0.0/proto"
+rm protovalidate.tar.gz
+rm -fr protovalidate-testing
 
 # Generate gRPC service code
-protoc -I=${GOOGLEAPIS_DIR} --proto_path=proto --go_out=./generated/go  --go_opt paths=source_relative --go-grpc_out=./generated/go --go-grpc_opt paths=source_relative csv.proto
+protoc -I=googleapis -I=protovalidate --proto_path=proto --go_out=./generated/go  --go_opt paths=source_relative --go-grpc_out=./generated/go --go-grpc_opt paths=source_relative csv.proto
