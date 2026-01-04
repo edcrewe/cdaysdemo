e4ec199e752e737b1edf6e7674c35ecd517590c4
edcrewe <ed.crewe@enterprisedb.com>
2026-01-04 17:13:03 +0000

    use different paths for stream and plain

--- cmd/server/get_sample_csv.sh
+++ cmd/server/get_sample_csv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Download and create a 3Gb sample CSV file for testing 
+# Download and create a 1.4 Gb sample CSV file for testing 
 # from data.gov.uk CKAN archive of UK Land Registry Price Paid Data
-for i in {1995..2010}
+for i in {1995..2002}
 do
    wget http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-$i.csv -O part_$i.csv
    cat part_*.csv >> sample_big.csv
